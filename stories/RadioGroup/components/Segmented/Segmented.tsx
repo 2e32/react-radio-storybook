68a05f85d99469d4bb28e722df8e9c0173a08bc3
konstantin b. <bogucharov.dev@gmail.com>
2025-03-17 23:18:44 +0300

    Fix Segmented types

--- stories/RadioGroup/components/Segmented/Segmented.tsx
+++ stories/RadioGroup/components/Segmented/Segmented.tsx
@@ -1,35 +1,41 @@
+import { useCallback } from 'react';
 import cn from 'classnames';
 
-import Radio from '@2e32/react-radio';
+import Radio, { type RadioGroupProps, type RenderOptionProps } from '@2e32/react-radio';
 
 import './styles.css';
 
-const Segmented = ({ value, options = [], onChange, ...rest }: any) => {
+const Segmented = ({ value, options = [], onChange }: RadioGroupProps<string, string>) => {
+  const renderOption = useCallback(
+    ({ option, optionProps }: RenderOptionProps<string>) => {
+      const { selected } = optionProps;
+
+      return (
+        <label
+          key={option}
+          className={cn('segmented-list__item', {
+            'segmented-list__item--checked': selected,
+          })}
+        >
+          <input
+            type="radio"
+            checked={selected}
+            className="segmented-list__input"
+            onChange={(e) => onChange?.(option, e)}
+          />
+          {option}
+        </label>
+      );
+    },
+    [onChange]
+  );
+
   return (
     <Radio.Group
       value={value}
       options={options}
       className="segmented-list"
-      renderOption={({ option, optionProps }) => {
-        const { selected } = optionProps;
-
-        return (
-          <label
-            key={option}
-            className={cn('segmented-list__item', {
-              'segmented-list__item--checked': selected,
-            })}
-          >
-            <input
-              type="radio"
-              checked={selected}
-              className="segmented-list__input"
-              onChange={(e) => onChange(option, e)}
-            />
-            {option}
-          </label>
-        );
-      }}
+      renderOption={renderOption}
     />
   );
 };
