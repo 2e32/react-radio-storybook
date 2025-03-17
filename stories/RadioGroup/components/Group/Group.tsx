c3d8b577e36119b28dd4715253982e9632c76cc8
konstantin b. <bogucharov.dev@gmail.com>
2025-03-17 23:16:27 +0300

    Fix Group types

--- stories/RadioGroup/components/Group/Group.tsx
+++ stories/RadioGroup/components/Group/Group.tsx
@@ -1,35 +1,41 @@
+import { useCallback } from 'react';
 import cn from 'classnames';
 
-import Radio from '@2e32/react-radio';
+import Radio, { type RadioGroupProps, type RenderOptionProps } from '@2e32/react-radio';
 
 import './styles.css';
 
-const Group = ({ value, options, disabled = false, onChange, ...rest }: any) => {
+const Group = ({ value, options, onChange }: RadioGroupProps<string, string>) => {
+  const renderOption = useCallback(
+    ({ option, optionProps }: RenderOptionProps<string>) => {
+      const { selected } = optionProps;
+
+      return (
+        <label
+          key={option}
+          className={cn('group-list__item', {
+            'group-list__item--checked': selected,
+          })}
+        >
+          {option}
+          <input
+            checked={selected}
+            type="radio"
+            className="checked__item__input"
+            onChange={(e) => onChange?.(option, e)}
+          />
+        </label>
+      );
+    },
+    [onChange]
+  );
+
   return (
     <Radio.Group
       value={value}
       options={options}
       className="group-list"
-      renderOption={({ option, optionProps }) => {
-        const { selected } = optionProps;
-
-        return (
-          <label
-            key={option}
-            className={cn('group-list__item', {
-              'group-list__item--checked': selected,
-            })}
-          >
-            {option}
-            <input
-              type="radio"
-              checked={selected}
-              className="checked__item__input"
-              onChange={(e) => onChange(option, e)}
-            />
-          </label>
-        );
-      }}
+      renderOption={renderOption}
     />
   );
 };
