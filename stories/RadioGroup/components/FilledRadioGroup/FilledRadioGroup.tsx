a18fd6a557444d08fb24deab8eb883a774622435
konstantin b. <bogucharov.dev@gmail.com>
2025-04-01 10:22:11 +0300

    Add props type

--- stories/RadioGroup/components/FilledRadioGroup/FilledRadioGroup.tsx
+++ stories/RadioGroup/components/FilledRadioGroup/FilledRadioGroup.tsx
@@ -5,7 +5,9 @@ import Radio, { type RadioGroupProps, type RenderOptionProps } from '@2e32/react
 
 import './styles.css';
 
-const FilledRadioGroup = ({ value, options = [], onChange }: RadioGroupProps<string, string>) => {
+type FilledRadioGroupProps = Omit<RadioGroupProps<string, string>, 'className' | 'renderIcon'>;
+
+const FilledRadioGroup = ({ onChange, ...rest }: FilledRadioGroupProps) => {
   const renderOption = useCallback(
     ({ option, optionProps }: RenderOptionProps<string>) => {
       const { selected } = optionProps;
@@ -30,14 +32,7 @@ const FilledRadioGroup = ({ value, options = [], onChange }: RadioGroupProps<str
     [onChange]
   );
 
-  return (
-    <Radio.Group
-      value={value}
-      options={options}
-      className="filled-radio-group"
-      renderOption={renderOption}
-    />
-  );
+  return <Radio.Group {...rest} className="filled-radio-group" renderOption={renderOption} />;
 };
 
 export default FilledRadioGroup;
