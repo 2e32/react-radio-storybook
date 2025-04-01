2effc12db3988cecf027061afb0fe9ddcdfc7fb8
konstantin b. <bogucharov.dev@gmail.com>
2025-04-01 09:36:44 +0300

    Rename to OutlinedRadioGroup

--- stories/RadioGroup/components/index.ts
+++ stories/RadioGroup/components/index.ts
@@ -1,6 +1,6 @@
 export { default as AlignmentRadio } from './AlignmentRadio';
 export { default as BorderedRadio } from './BorderedRadio';
 export { default as Checked } from './Checked';
-export { default as Group } from './Group';
+export { default as OutlinedRadioGroup } from './OutlinedRadioGroup';
 export { default as Segmented } from './Segmented';
 export { default as Tabs } from './Tabs';
